4f235f9aa9e8bdbbfbe12f6e45593b0cd2fc9f51
Yassine Kamouss <156694023+yassinekamouss@users.noreply.github.com>
2026-01-16 01:57:03 +0100

    Change availability zones from eu-north to eu-west

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1a"
+  availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name                                          = "public-eu-north-1a"
+    Name                                          = "public-eu-west-1a"
     "kubernetes.io/role/elb"                      = "1"
     "kubernetes.io/cluster/estate-rental-cluster" = "shared"
   }
@@ -31,9 +31,9 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "eu-north-1a"
+  availability_zone = "eu-west-1a"
   tags = {
-    Name                                          = "private-eu-north-1a"
+    Name                                          = "private-eu-west-1a"
     "kubernetes.io/role/internal-elb"             = "1"
     "kubernetes.io/cluster/estate-rental-cluster" = "shared"
   }
@@ -43,10 +43,10 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-north-1b"
+  availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name                                          = "public-eu-north-1b"
+    Name                                          = "public-eu-west-1b"
     "kubernetes.io/role/elb"                      = "1"
     "kubernetes.io/cluster/estate-rental-cluster" = "shared"
   }
@@ -55,9 +55,9 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "eu-north-1b"
+  availability_zone = "eu-west-1b"
   tags = {
-    Name                                          = "private-eu-north-1b"
+    Name                                          = "private-eu-west-1b"
     "kubernetes.io/role/internal-elb"             = "1"
     "kubernetes.io/cluster/estate-rental-cluster" = "shared"
   }
